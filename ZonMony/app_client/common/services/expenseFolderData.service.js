d5ffc1691358c91806bf742bb698865b61a32c3a
THBar <choreno@gmail.com>
2016-09-13 16:51:03 -0400

    updating angular serivce to get the total expenses and current DTTM expenses

---
+++ ZonMony/app_client/common/services/expenseFolderData.service.js
@@ -0,0 +1,39 @@
+﻿angular.module('zonMonyApp')
+	.service('expenseFolderData', expenseFolderData);
+
+function expenseFolderData($http) {
+
+	//This service need to have such capabilities, 
+	// 1. provides total expenses($$$) from start year to until now
+	// 2. provides expense data for current month and year
+	// 3. provides unique folder list
+
+	//get all expenses
+	var getAllExpenses = function () {
+		return $http.get('/api');
+	};
+
+
+	//add more services from here !!!!!!!!  ---------------------------------------------------
+
+	//get current DTTM expenses
+	var getCurrentExpenses = function (year, month) {
+
+		return $http.get('/api/folder?year=' + year + '&month=' + month);
+
+	};
+
+
+	//Return all services menu !!
+
+	return {
+		getAllExpenses: getAllExpenses,
+		getCurrentExpenses: getCurrentExpenses
+	};
+
+
+
+
+}
+
+
--- ZonMony/app_client/home/home.controller.js
+++ ZonMony/app_client/home/home.controller.js
@@ -1,33 +1,62 @@
 ﻿angular.module('zonMonyApp')
-	.controller('homeCtrl', homeCtrl);
+	.controller('homeCtrl', homeCtrl)
 
 
 
-function homeCtrl(expenseData) {
+function homeCtrl(expenseFolderData) {
+
 	var vm = this;
 
 	vm.pageHeader = 'ttt';
 
 	vm.message = 'Checking your expenses...';
 
+
 	//Alert!, $http.get returns a promise with two methods, success and error
-	expenseData
+	expenseFolderData.getAllExpenses()
 		.success(function (data) {
-			vm.expenses = data;
 
-			//ES6 way -- getting the unique folders only
-			vm.folders = [...new Set(data.map(x => x.name))].sort(); 
+			//calculate total expenses, -- do not add paybycreditcard items
+			let sum = 0 ; 
+			for(let i in data){
+				console.log(data[i].payByCreditCard);
+				if(data[i].payByCreditCard == false){
+				for(let j in data[i].payment){
+					sum = sum+data[i].payment[j].amount ;
+				}
+				}
+			}
+			console.log(sum);
 
-			//reprocessing the expense data
-			//to get the only docs for current month and year as the initial UI !!!!
-			//like what, 
-			//need to check api to /api?month=9&year=2016  
+			vm.total = sum ;  
 
+			vm.pageHeader = '$' + sum ; 
+			
 
 		})
 		.error(function (err) {
 			console.log(err);
 		});
+
+
+
+
+	var now = new Date();
+	// var year = now.getFullYear();
+	// var month = now.getMonth(); 
+	var year = 2015;
+	var month = 1;
+
+	expenseFolderData.getCurrentExpenses(year, month)
+		.success(function (data) {
+			vm.expenses = data;
+			vm.folders = [...new Set(data.map(x => x.name))].sort();
+		})
+		.error(function (err) {
+			console.log(err);
+		})
+
+
 
 
 }
